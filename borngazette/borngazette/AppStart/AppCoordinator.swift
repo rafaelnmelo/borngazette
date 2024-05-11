cb4cc6cd3e66c8b21c142cf4edd0bd9340a36248
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-05-11 14:42:31 -0300

    Heranças, coordinator, launchscreen

--- borngazette/borngazette/AppStart/AppCoordinator.swift
+++ borngazette/borngazette/AppStart/AppCoordinator.swift
@@ -5,4 +5,24 @@
 //  Created by Rafael Melo on 11/05/24.
 //
 
-import Foundation
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: BaseNavigationController { get set }
+}
+
+class AppCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: BaseNavigationController
+    
+    init(navigationController: BaseNavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = GazetteViewController(nibName: nil, bundle: nil)
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
--- borngazette/borngazette/Controllers/Gazette/GazetteViewController.swift
+++ borngazette/borngazette/Controllers/Gazette/GazetteViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GazetteViewController: UIViewController {
+class GazetteViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
